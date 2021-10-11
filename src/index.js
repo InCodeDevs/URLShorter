7eb4a5731640ea2daf67534152910c2ebd53c659
MCTzOCK <dev@mctzock.de>
2021-10-11 10:07:32 +0200

    Changed base path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@
  * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
  */
 
-process.env.URL_PUB_PATH = "./url-public";
+const os = require('os')
+const path = require("path");
+
+process.env.URL_PUB_PATH = path.join(os.homedir(), "./.incode-urls");
 
 const urlServer = require('./module/urlshorter');
 const sampleApp = require('./module/sampleApp');
