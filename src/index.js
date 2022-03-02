c96774dc63fbd9e7a4982a132128a0ab62f44259
MCTzOCK <dev@mctzock.de>
2022-03-02 16:04:27 +0100

    Updated PRIVATE path

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@
 
 const os = require('os')
 const path = require("path");
+const fs = require("fs");
 
-process.env.URL_PUB_PATH = path.join(os.homedir(), "./.incode-urls");
+process.env.URL_PUB_PATH = path.join(os.homedir(), ".incode", "urls");
+
+if(!fs.existsSync(process.env.URL_PUB_PATH)){
+    fs.mkdirSync(process.env.URL_PUB_PATH);
+}
 
 const urlServer = require('./module/urlshorter');
 const sampleApp = require('./module/sampleApp');
